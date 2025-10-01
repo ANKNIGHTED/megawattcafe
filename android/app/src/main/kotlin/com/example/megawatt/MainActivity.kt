2e81ac16dea0b2d7377e5934bd6e4a0a89472665
anknighted <becyabbie@gmail.com>
2025-10-01 12:51:28 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/megawatt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.megawatt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Appcolors extends StatelessWidget {
+  const Appcolors({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
